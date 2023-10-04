d1ac23768d45d9995bc8254ed125ca28064bb4eb
AbdessamadPas <boughadoin@gmail.com>
2023-10-04 19:51:59 +0100

    home and trackListItem �️�️

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,20 @@
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
+import {tracks} from '../../assets/data/tracks'
 import { Text, View } from '../../components/Themed';
+import { FlatList } from 'react-native-gesture-handler';
+import TrackListItem from '../../components/trackListItem';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <FlatList
+        data={tracks}
+        renderItem={({item})=>(
+          <TrackListItem track={item}/>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+  
     </View>
   );
 }
---
+++ components/trackListItem.tsx
@@ -0,0 +1,48 @@
+import { View, Text , Image,StyleSheet } from 'react-native'
+import React from 'react'
+import { Track } from '../constants/types'
+
+type TrackProps = {
+  track : Track
+}
+
+const TrackListItem = ({track} :TrackProps ) => {
+    return (
+    <View style={styles.container}>
+      <View>
+        <Image style={styles.image} source={{uri : track.album.images[0].url}} />
+      </View>
+      <View>
+        <Text style={styles.title}>{track.name}</Text>
+        <Text style={styles.subtitle}>{track.artists[0].name}</Text>
+      </View>
+    </View>
+  )
+}
+
+export default TrackListItem
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    flexDirection : 'row',
+    alignItems : 'center',
+    padding:5,
+    gap : 20,
+
+    marginVertical : 5,
+  },
+  image : {
+    width : 50,
+    aspectRatio : 1,
+    borderRadius : 5,
+  },
+  title : {
+    color : 'white',
+    fontSize : 16,
+    fontWeight : 'bold',
+  },
+  subtitle : {
+    color : 'lightgray',
+    fontSize : 14,
+  }
+})
