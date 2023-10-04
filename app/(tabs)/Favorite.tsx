8e26ddc791bbf8bb60f5e67229047d33e803a0ac
AbdessamadPas <boughadoin@gmail.com>
2023-10-04 21:11:22 +0100

    search and favorate screen

--- app/(tabs)/Favorite.tsx
+++ app/(tabs)/Favorite.tsx
@@ -1,30 +1,57 @@
-import React from 'react'
+import {useState} from 'react'
 import { StyleSheet } from 'react-native';
+import {tracks} from '../../assets/data/tracks'
 import { Text, View } from '../../components/Themed';
-
-const Favorite = () => {
+import { FlatList } from 'react-native-gesture-handler';
+import TrackListItem from '../../components/trackListItem';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {FontAwesome} from '@expo/vector-icons';
+import { TextInput } from 'react-native';
+export default function FavorateScreen() {
+  const [search, setSearch] = useState('')
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>favoritess</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-    </View>
-  )
-}
-export default Favorite
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-    },
-    separator: {
-      marginVertical: 30,
-      height: 1,
-      width: '80%',
-    },
-  });
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchBarContainer}>
+        <FontAwesome  name= 'search' size={16} color='gray'/>
+        <TextInput value={search} 
+          onChange={()=>setSearch("")}
+        style={styles.searchBar} placeholder='search in favorites...'/>
+        <Text> Cancel</Text>
+      </View>
+      <FlatList
+        data={tracks}
+        renderItem={({item})=>(
+          <TrackListItem track={item}/>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
   
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  searchBarContainer : {
+    flexDirection : 'row',
+    alignItems : 'center',
+    gap : 10,
+    width : '95%',
+    padding : 10,
+    borderRadius : 5,
+    margin : 10,
+  },
+  searchBar : {
+    flex : 1,
+    backgroundColor : '#282828',
+    padding : 3,
+    borderRadius  : 5,
+    color : 'white'
+
+  }
+});
